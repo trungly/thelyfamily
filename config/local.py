79dce2c099bdb816ca8caf65fb36c784a9df92ab
Trung Ly <tly@truecar.com>
2014-01-12 02:10:43 -0800

    This commit only for appengine branch: check in config files

---
+++ config/local.py
@@ -0,0 +1,6 @@
+# Instagram
+REDIRECT_URI = 'http://localhost:8080/photos/return'
+CLIENT_ID = '2dfd34760647442e879b7fd6ca1201a9'
+CLIENT_SECRET = '8f321a66d0c94d5c8808eff30906ddcf'
+#Todo: convert this to a BASE_URL. Then fill in client_id and redirect_uri from above values
+INSTAGRAM_AUTH_URL = 'https://instagram.com/oauth/authorize/?client_id=2dfd34760647442e879b7fd6ca1201a9&redirect_uri=http://localhost:8080/photos/return&response_type=code'
