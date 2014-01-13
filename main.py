e36a2d1f3f71763b098acfa5671b93bd1836e957
Trung Ly <tly@truecar.com>
2014-01-12 21:42:21 -0800

    hard-code config files for now because GAE deploy is not correctly putting config.production there

--- main.py
+++ main.py
@@ -13,7 +13,14 @@ from app.forms import SiteMemberForm
 
 app = Flask(__name__.split('.')[0])
 
-app.config.from_object('config.production')
+# app.config.from_object('config.production')REDIRECT_URI = 'http://beta.thelyfamily.com/photos/return'
+app.config.update(
+    SECRET_KEY='\xe90\xa0K`\xe0\x19\xdezu/\xc6\x81\xc3\xc1\xe3\xe7\xc5\xdbx\xe1\x9a\xebm',
+    REDIRECT_URI='http://beta.thelyfamily.com/photos/return',
+    CLIENT_ID='9bd63a0b912c4e7f8c8e979276e1cff5',
+    CLIENT_SECRET='9fe9f63f65644d2a8fa68740e70f9a9a',
+)
+
 settings = os.environ.get('APP_SETTINGS')
 if 'localhost' in os.environ.get('SERVER_NAME'):
     app.config.from_object('config.local')
