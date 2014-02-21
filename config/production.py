4b901f0f00e275e7705920ae2576748cc170de84
Trung Ly <tly@truecar.com>
2014-02-20 21:17:12 -0800

    try a different Facebook app_id/secret

--- config/production.py
+++ config/production.py
@@ -7,7 +7,7 @@ INSTAGRAM_CLIENT_ID = '9bd63a0b912c4e7f8c8e979276e1cff5'
 INSTAGRAM_CLIENT_SECRET = '9fe9f63f65644d2a8fa68740e70f9a9a'
 
 # Facebook
-FACEBOOK_APP_ID = '434962266536461'
-FACEBOOK_SECRET_KEY = '0c5e486681620e90babf6fd9550dd97d'
-FACEBOOK_APP_TOKEN = '434962266536461|NJ6i3pl4Cowa14_ah6WvuOOTFrY'  # obtained by going to:
-# https://graph.facebook.com/oauth/access_token?client_id=434962266536461&client_secret=0c5e486681620e90babf6fd9550dd97d&grant_type=client_credentials
+FACEBOOK_APP_ID = '1408819302707661'
+FACEBOOK_SECRET_KEY = '7f00476bf9f06af27796ec4aebe62339'
+FACEBOOK_APP_TOKEN = '1408819302707661|EzmoyrPqydy4XImhr5fCtjRIVfY'  # obtained by going to:
+# https://graph.facebook.com/oauth/access_token?client_id=1408819302707661&client_secret=7f00476bf9f06af27796ec4aebe62339&grant_type=client_credentials
