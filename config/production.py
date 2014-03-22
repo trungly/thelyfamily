a6f7773ca0cfb58a632ba6eb067dd1e174f50639
Trung Ly <tly@truecar.com>
2014-03-22 11:43:51 -0700

    update values for migration to thelyfamily.com

--- config/production.py
+++ config/production.py
@@ -1,13 +1,13 @@
 SECRET_KEY = '\xe90\xa0K`\xe0\x19\xdezu/\xc6\x81\xc3\xc1\xe3\xe7\xc5\xdbx\xe1\x9a\xebm'
 
-HOST_NAME = 'beta.thelyfamily.com'
+HOST_NAME = 'thelyfamily.com'
 
 # Instagram
-INSTAGRAM_CLIENT_ID = '9bd63a0b912c4e7f8c8e979276e1cff5'
-INSTAGRAM_CLIENT_SECRET = '9fe9f63f65644d2a8fa68740e70f9a9a'
+INSTAGRAM_CLIENT_ID = 'fa6e5a2ef09d41ef94433fb2379d672b'
+INSTAGRAM_CLIENT_SECRET = '6c561122b3654749ae9e77b2a569ece5'
 
 # Facebook
-FACEBOOK_APP_ID = '1408819302707661'
-FACEBOOK_APP_SECRET = '7f00476bf9f06af27796ec4aebe62339'
-FACEBOOK_ACCESS_TOKEN = '1408819302707661|EzmoyrPqydy4XImhr5fCtjRIVfY'  # obtained by going to:
-# https://graph.facebook.com/oauth/access_token?client_id=1408819302707661&client_secret=7f00476bf9f06af27796ec4aebe62339&grant_type=client_credentials
+FACEBOOK_APP_ID = '434962266536461'
+FACEBOOK_APP_SECRET = '0c5e486681620e90babf6fd9550dd97d'
+FACEBOOK_ACCESS_TOKEN = '434962266536461|NJ6i3pl4Cowa14_ah6WvuOOTFrY'  # obtained by going to:
+# https://graph.facebook.com/oauth/access_token?client_id=434962266536461&client_secret=0c5e486681620e90babf6fd9550dd97d&grant_type=client_credentials
